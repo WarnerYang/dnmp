5b52921c9e22d738d4ca2735064badb467efc982
yhq <yanghuaqiang@qhweidai.com>
2019-03-25 16:39:12 +0800

    1

--- www/localhost/1.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-//每隔2000ms触发一次
-swoole_timer_tick(1000, function ($timer_id) {
-    // echo $timer_id;
-    $txt = file_get_contents('2.txt')?:'iushfisfhi';
-    file_put_contents('2.txt', $txt, FILE_APPEND);
-    // file_put_contents('1.txt', "$num\n",FILE_APPEND);
-});
